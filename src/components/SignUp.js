a8b2a95aff4eb35303d80ec741bb4222ed32ad77
Jacqueline Nguyen <jacqueline.nguyen.41@gmail.com>
2020-03-06 21:55:52 +0100

    added others inputs + handleSubmit function

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,21 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SignUp extends Component {
     //STATE
     state = {
-        email: 'There is nothing'
+        email: "mon@email.com",
+        password: "monPassw0rd",
+        name: "James",
+        lastname: "Bond"
     }
+
     render() { //FONCTIONS
+
+        //update the form
         const updateEmailField = (e) => {
-            this.setState({email: e.target.value})
+            this.setState({ email: e.target.value })
         }
-        return(
+        const updatePasswordField = (e) => {
+            this.setState({ password: e.target.value })
+        }
+        const updateNameField = (e) => {
+            this.setState({ name: e.target.value })
+        }
+        const updateLastNameField = (e) => {
+            this.setState({ lastname: e.target.value })
+        }
+
+        //send the form
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            console.log(this.state)
+        }
+
+        return (
             <div>
-                <h1>{this.state.email}</h1>
-                <input type='email' 
-                name='email'
-                onChange={ (e) => updateEmailField(e)}
-                 />
+                <form onSubmit={handleSubmit}>
+                    <h1>{JSON.stringify(this.state)}</h1>
+                    <input type='email'
+                        name='email'
+                        onChange={(e) => updateEmailField(e)}
+                    />
+                    <input type='password'
+                        name='password'
+                        onChange={(e) => updatePasswordField(e)}
+                    />
+                    <input type='password'
+                        name='passwordbis'
+                    //onChange={ (e) => updateEmailField(e)}
+                    />
+                    <input type='text'
+                        name='name'
+                        onChange={(e) => updateNameField(e)}
+                    />
+                    <input type='text'
+                        name='lastname'
+                        onChange={(e) => updateLastNameField(e)}
+                    />
+                    <input type="submit" value="Soumettre" />
+
+                </form>
+
             </div>
         )
     }
